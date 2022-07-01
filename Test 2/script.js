52ceeb821ea5b70b5764d0276e2cb3066a72100a
sebastianprieto1 <105680771+sebastianprieto1@users.noreply.github.com>
2022-07-01 12:07:30 +1000

    Commented out mock cards git push

--- Test 2/script.js
+++ Test 2/script.js
@@ -1,37 +1,21 @@
-// const projectName = document.getElementById('projectName');
-// const projectDeadline = document.getElementById('projectDeadline');
-// const projectDescription = document.getElementById('projectDescription');
-// const projectForm = document.getElementById('projectForm');
-// const errorElement = document.getElementById('error');
 
-// projectForm.addEventListener('submit', (e) => {
-//     let messages = [];
-//     if (projectName.value === '' || projectName.value === null) {
-//         messages.push('Project name is required');
-//         }  
-        
-//     if (messages.length > 0) {
-//         e.preventDefault();
-//         errorElement.innerText = messages.join(', ');
-//     }
-// })
 
-const name = document.getElementById('projectName');
-const description = document.getElementById('projectDescription');
+const pName = document.getElementById('projectName');
+const pDescription = document.getElementById('projectDescription');
 const form = document.getElementById('form');
 const errorElement = document.getElementById('error');
+messages.push('Please enter a description for your project')
 
 form.addEventListener('submit', (e) => {
     let messages = []
-    if (name.value === '' || name.value === null) { 
+    if (pName.value === '' || pName.value === null) { 
         messages.push('Name is required')
     }
 
-    if (projectDescription.value === '' || projectDescription.value === null) {
-        messages.push('Project description is required')
+    if (pDescription.value === '' || pDescription.value === null) {
     }
+
     if (messages.length > 0) {
-    e.preventDefault()
     errorElement.innerText = messages.join(', ')
     } 
 })
